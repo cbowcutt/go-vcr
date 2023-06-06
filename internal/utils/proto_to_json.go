167c5fdada654ee20669245b1b3866065c0b345e
cbowcutt <bowcuttchris@gmail.com>
2023-06-05 22:05:07 -0400

    test commit

--- internal/utils/proto_to_json.go
+++ internal/utils/proto_to_json.go
@@ -9,7 +9,7 @@ func ProtoToJson(r interface{}) (string, error) {
 	reqProto := g.RawRequest.(proto.Message)
 	jsonBytes, err := protojson.Marshal(reqProto)
 	if err != nil {
-		return nil, err
+		return "nil", err
 	}
 	return string(jsonBytes), err
 }
