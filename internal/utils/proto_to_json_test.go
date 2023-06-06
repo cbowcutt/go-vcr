b057a434c3359ea8f2ca1e99b6578105c706d54b
cbowcutt <bowcuttchris@gmail.com>
2023-06-06 09:08:23 -0400

    make unit test better

--- internal/utils/proto_to_json_test.go
+++ internal/utils/proto_to_json_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"encoding/json"
 	"github.com/cbowcutt/go-vcr/testing/fixtures"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/types/known/wrapperspb"
@@ -13,5 +14,10 @@ func Test_ProtoToJson(t *testing.T) {
 		GoogleString: &wrapperspb.StringValue{Value: "someString"}}
 	jsonString, err := ProtoToJson(protoRecord)
 	assert.Nil(t, err)
-	assert.Equal(t, `{"key":"Hello","value":"Friend","googleString":"someString"}`, jsonString)
+	jsonMap := make(map[string]interface{})
+	err = json.Unmarshal([]byte(jsonString), &jsonMap)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello", jsonMap["key"].(string))
+	assert.Equal(t, "Friend", jsonMap["value"].(string))
+	assert.Equal(t, "someString", jsonMap["googleString"].(string))
 }
