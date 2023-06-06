e3991996ef53d2722b2decf854535e763e1d18c0
cbowcutt <bowcuttchris [at] gmail.com>
2023-06-05 21:52:27 -0400

    some changes

--- internal/utils/proto_to_json.go
+++ internal/utils/proto_to_json.go
@@ -1,3 +1,10 @@
+package utils
+
+import (
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
 func ProtoToJson(r interface{}) (string, error) {
 	reqProto := g.RawRequest.(proto.Message)
 	jsonBytes, err := protojson.Marshal(reqProto)
